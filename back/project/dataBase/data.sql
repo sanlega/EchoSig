8450763f81900e5b49d912bc994e89661a829be5
luissoy <luisserranomarin@gmail.com>
2023-05-06 20:19:33 +0200

    feat: Tables script
    Tables script

--- back/project/dataBase/data.sql
+++ back/project/dataBase/data.sql
@@ -1,13 +1,43 @@
 CREATE DATABASE IF NOT EXISTS `echoSig`;
 USE `echoSig`;
 
-CREATE TABLE `Customers` (
-                             `CustomerName` varchar(255) DEFAULT NULL,
-                             `ContactName` varchar(255) DEFAULT NULL,
-                             `Address` varchar(255) DEFAULT NULL,
-                             `City` varchar(255) DEFAULT NULL,
-                             `PostalCode` varchar(255) DEFAULT NULL,
-                             `Country` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE User (
+                      userId INT NOT NULL AUTO_INCREMENT,
+                      username VARCHAR(50) NOT NULL,
+                      email VARCHAR(100) NOT NULL,
+                      password VARCHAR(255) NOT NULL,
+                      PRIMARY KEY (userId)
+);
 
-INSERT INTO `Customers` VALUES ('Cardinal','Tom B. Erichsen','Skagen 21','Stavanger','4006','Norway'),('Wilman Kala','Matti Karttunen','Keskuskatu 45','Helsinki','21240','Finland');
+CREATE TABLE Type (
+                      typeId INT NOT NULL AUTO_INCREMENT,
+                      destinationId INT NOT NULL,
+                      message VARCHAR(255),
+                      PRIMARY KEY (typeId),
+                      FOREIGN KEY (destinationId) REFERENCES Destination(destinationId)
+);
+
+CREATE TABLE Destination (
+                             destinationId INT NOT NULL AUTO_INCREMENT,
+                             service VARCHAR(50) NOT NULL,
+                             endpoint VARCHAR(255) NOT NULL,
+                             PRIMARY KEY (destinationId)
+);
+
+CREATE TABLE Node (
+                      nodeId INT NOT NULL AUTO_INCREMENT,
+                      userId INT NOT NULL,
+                      typeId INT NOT NULL,
+                      address VARCHAR(255),
+                      PRIMARY KEY (nodeId),
+                      FOREIGN KEY (userId) REFERENCES User(userId),
+                      FOREIGN KEY (typeId) REFERENCES Type(typeId)
+);
+
+CREATE TABLE Event (
+                       eventId INT NOT NULL AUTO_INCREMENT,
+                       nodeId INT NOT NULL,
+                       time DATETIME NOT NULL,
+                       PRIMARY KEY (eventId),
+                       FOREIGN KEY (nodeId) REFERENCES Node(nodeId)
+);
